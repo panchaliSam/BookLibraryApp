d33d62db08aeabbf52d09da2917a67694944eb40
panchaliSam <panchalisam20030117@gmail.com>
2024-05-09 18:47:34 +0530

    implemnt update delete dialogue

--- app/src/main/java/com/example/booklibraryapp/MainActivity.kt
+++ app/src/main/java/com/example/booklibraryapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,9 +23,30 @@ class MainActivity : AppCompatActivity() {
 
     //method to save records in database
     fun saveRecord(view: View){
+        //Retrieve input values from EditText fields
         val id = bookIdEditText.text.toString()
         val bookName = bookNameEditText.text.toString()
         val authorName = bookAuthorNameText.text.toString()
-        //val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+
+        //Create an instance of the DatabaseHandler class
+        val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+
+        //Check if input fields are not empty
+        if(id.trim() != "" && bookName.trim() != "" && authorName.trim() != ""){
+            //call addBook method
+            val status = databaseHandler.addBook(BookModelClass(id,bookName,authorName))
+            //If record is added successfully, show a toast message
+            if(status > -1){
+                Toast.makeText(applicationContext, "Record saved", Toast.LENGTH_LONG).show()
+                //Clear EditText fields
+                bookIdEditText.text.clear()
+                bookNameEditText.text.clear()
+                bookAuthorNameText.text.clear()
+            }else{
+                // If any field is empty, show an error message
+                Toast.makeText(applicationContext, "ID, name, or email cannot be blank", Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
